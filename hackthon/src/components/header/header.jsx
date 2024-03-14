ea2bcf6e5d4418714c8f32d91e1c191085b5842a
Yogurt7v <egorov-2k@yandex.ru>
2024-03-15 00:27:09 +0300

    header

--- hackthon/src/components/header/header.jsx
+++ hackthon/src/components/header/header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <>
       <div className={slyle.header}>
-        <h1>Hackathon 2.0</h1>
+        <h1>BrainStorm Hackathon 2.0</h1>
         <ul className={slyle.teamList}>
           {TEAM.map((item) => (
             <li key={item.id} className={slyle.teamItem}>
