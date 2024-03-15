2207647a675e5765f8494511a967b94944e2974f
NemesisWar <122649498+Holyhelper1@users.noreply.github.com>
2024-03-15 13:20:42 +0300

    Подкорректировал хэдэр, добавил немного анимации и пнг для избранного

--- hackthon/src/components/header/header.jsx
+++ hackthon/src/components/header/header.jsx
@@ -1,22 +1,23 @@
 import slyle from "./header.module.css";
 import { TEAM } from "../../constants/team";
-
+import star from "./star.png";
 
 export const Header = () => {
-
-
   return (
     <>
       <div className={slyle.header}>
         <h1>BrainStorm Hackathon 2.0</h1>
         <ul className={slyle.teamList}>
           {TEAM.map((item) => (
-            <li key={item.id} className={slyle.teamItem}>
-              <p>{item.name}</p>
+            <li key={item.id} >
+              <p className={slyle.names}>{item.name}</p>
             </li>
           ))}
         </ul>
-        <div>Избранное</div>
+        <div className={slyle.favorites}>
+          <img className={slyle.starspiner} src={star} alt="Избранное"/>
+          <h3>Избранное</h3>
+        </div>
       </div>
     </>
   );
