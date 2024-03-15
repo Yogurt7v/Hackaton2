66fa285045edd9323be8107061f93503a13f1751
Yogurt7v <egorov-2k@yandex.ru>
2024-03-15 11:55:01 +0300

    swiper готов

--- hackthon/src/components/swiper/GitSwip.jsx
+++ hackthon/src/components/swiper/GitSwip.jsx
@@ -1,26 +1,17 @@
-/* eslint-disable react/prop-types */
+import style from "./GitSwip.module.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import { useRef } from 'react';
-import style from './GitSwip.module.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-export const GitSwip = ({githubRepos}) => {
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-    const progressCircle = useRef(null);
-    const progressContent = useRef(null);
-    const onAutoplayTimeLeft = (s, time, progress) => {
-        progressCircle.current.style.setProperty('--progress', 1 - progress);
-        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`
-    };
+export const GitSwip  =({githubRepos}) => {
 
-    console.log(githubRepos);
-
-    return (
-        <>
-        <Swiper
+  console.log(githubRepos);
+  return (
+    <>
+      <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -32,19 +23,11 @@ export const GitSwip = ({githubRepos}) => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
         className={style.mySwiper}
       >
         {githubRepos.map((item) => (
-          <SwiperSlide key={item.id}>{item.name}</SwiperSlide>
-        ))}
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            {/* <circle cx="24" cy="24" r="20"></circle> */}
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
+  <SwiperSlide key={item}>{item}</SwiperSlide>))}
       </Swiper>
-        </>
-    )
+    </>
+  );
 }
--- hackthon/src/constants/team.js
+++ hackthon/src/constants/team.js
@@ -1,27 +1,27 @@
  export const TEAM =  [
         {
             "id": "1",
-            "name": "Name",
-            "surname": "Surname",
+            "name": "Сергей",
+            "surname": "Егоров",
             "githubLogin": "Yogurt7v",
-            "description": "Team 1 description",
+            "description": "Team 2 hackathon 2",
             "htmlProgress": 90,
             "socialNetwork":{
-                "vk": "нету",
-                "facebook":"забанен",
-                "instagram": "заблокирован",
-                "linkedIn": "недоступен",
-                "github": "тест"
+                "vk": "none",
+                "facebook":"none",
+                "instagram": "none",
+                "linkedIn": "none",
+                "github": "https://github.com/Yogurt7v"
             },
             "special": ["teamLead", "frontend"],
             "image": "https://1.downloader.disk.yandex.ru/preview/09911ee2a64ba554d0b64995361a93600aa49a7cfc3d4a08e403c19c011a2953/inf/ZA5nVuIWol8rtRW1_HfPdplJHvOOZkt4rrVQabdAqPyfw7wGY0hT3Lg4WiZZyiAUgF8WC9HH5lMhtDw3lNqqBA%3D%3D?uid=87392891&filename=Image1.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=87392891&tknv=v2&size=3200x1752"
         },
         {
             "id": "2",
-            "name": "Name",
-            "surname": "Surname",
-            "description": "Team 1 description",
-            "htmlProgress": 90,
+            "name": "Александр",
+            "surname": "Шульгин",
+            "description": "Team 2 ackathon 2. Веду активный образ жизни,  решил сменить рабочую профессию и обучаюсь в Result School на Front-end-React разработчика. Люблю путешествовать, узнавать новое, создавать и делать полезные вещи и дела 🙂",
+            "htmlProgress": 91,
             "socialNetwork":{
                 "vk": "",
                 "facebook":"",
@@ -29,8 +29,8 @@
                 "linkedIn": "",
                 "github": ""
             },
-            "special": [],
-            "image": ""
+            "special": ["frontend"],
+            "image": "https://sun9-5.userapi.com/impg/XgW2ssX-L5NRdg9RVFTKGxxKdUXbIMKz5CpuWg/Hw9xx80-0xw.jpg?size=1620x2160&quality=96&sign=72646e592faa0b975137acc425ffbbc1&type=album"
         },
         {
             "id": "3",
