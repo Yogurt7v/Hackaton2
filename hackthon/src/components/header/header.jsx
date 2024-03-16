8716d3e3dac96a0b1fb392018bbb5c7a8c0f4b11
Anastasiya <anastasiya.ibr@mail.ru>
2024-03-17 00:31:15 +0300

    чуть стили добавила

--- hackthon/src/components/header/header.jsx
+++ hackthon/src/components/header/header.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
 import slyle from "./header.module.css";
-import brain from "../../assets/icons/brain.svg"
-import headerFavorite from "../../assets/icons/headerStar.svg"
+import brain from "../../assets/icons/brain.svg";
+import headerFavorite from "../../assets/icons/headerStar.svg";
 // import { TEAM } from "../../constants/team";
 
 export const Header = ({ onFavoritePage }) => {
   return (
     <>
       <div className={slyle.header}>
-        <div className={slyle.headerTitle}>BrainStorm <img src={brain} alt="brain"/></div>
+        <div className={slyle.headerTitle}>
+          BrainStorm <img src={brain} alt="brain" />
+        </div>
         <div className={slyle.headerTitle}>Hackthon 2.0</div>
+        <Link to="/testPage">Пройти тест на технологии</Link>
         {onFavoritePage ? null : (
           <Link to="/favourites">
-            <div className={slyle.headerTitle}>Избранное <img className={slyle.headerStar} src={headerFavorite} alt="headerFavorite"/></div>
+            <div className={slyle.headerTitle}>
+              Избранное{" "}
+              <img
+                className={slyle.headerStar}
+                src={headerFavorite}
+                alt="headerFavorite"
+              />
+            </div>
           </Link>
         )}
       </div>
