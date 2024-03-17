b5531558ea426035ccac2ab48756ded694455606
Yogurt7v <egorov-2k@yandex.ru>
2024-03-17 11:59:54 +0300

    some fix

--- hackthon/src/components/header/header.jsx
+++ hackthon/src/components/header/header.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import slyle from "./header.module.css";
 import brain from "../../assets/icons/brain.svg";
 import headerFavorite from "../../assets/icons/headerStar.svg";
-// import { TEAM } from "../../constants/team";
 
 export const Header = ({ onFavoritePage }) => {
   return (
@@ -11,8 +10,9 @@ export const Header = ({ onFavoritePage }) => {
         <div className={slyle.headerTitle}>
           BrainStorm <img src={brain} alt="brain" />
         </div>
-        <div className={slyle.headerTitle}>Hackthon 2.0</div>
+        <div className={slyle.headerTitle}>
         <Link to="/testPage">Пройти тест на технологии</Link>
+        </div>
         {onFavoritePage ? null : (
           <Link to="/favourites">
             <div className={slyle.headerTitle}>
